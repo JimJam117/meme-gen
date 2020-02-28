b30f96fb2c2330951b968bd331d557637bdabf38
jimjam117 <jamessparrow101@gmail.com>
2020-02-28 23:26:58 +0000

    Added styling

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -43,7 +43,7 @@ class MemeGenerator extends Component {
 
     render() {
         return (
-            <div>
+            <main>
                 <form>
                     <label>
                             Top Text
@@ -62,7 +62,7 @@ class MemeGenerator extends Component {
                     <h2 className="top">{this.state.topText}</h2>
                     <h2 className="bottom">{this.state.bottomText}</h2>
                 </div>
-            </div>
+            </main>
         )
     }
 }
